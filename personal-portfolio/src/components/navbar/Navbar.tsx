48076667537baae21bbd5958fc06aee41daa130f
Maicol Rojas <maidanlrojas@gmail.com>
2024-08-07 18:44:52 -0500

    Actualiza - Barra de navegacion

--- personal-portfolio/src/components/navbar/Navbar.tsx
+++ personal-portfolio/src/components/navbar/Navbar.tsx
@@ -1,23 +1,31 @@
 /// Estilos
 import "./Navbar.css"
 
+
 // Componente: Barra de navegacion
 export const Navbar = () => {
     return (
 
         <header className="row">
 
-            <div className="col-12 d-flex flex-row align-items-center justify-content-between header ">
-                <a className="header__title" href="/"> Maicol Rojas </a>
+            {/* Barra de navegacion */}
+            <div className="col-12 d-flex flex-row align-items-center justify-content-between header">
 
+                {/* Titulo pagina */}
+                <h1 className="header__title">
+                    <span>Maicol </span>
+                    <span className="header__title-hight">Rojas</span>
+                </h1>
+
+                {/* Redirecciones */}
                 <nav className="header__navbar">
                     <a href="/"> Inicio </a>
                     <a href="/"> Proyectos </a>
                     <a href="/"> Sobre Mi </a>
                     <a href="/"> Contacto </a>
                 </nav>
-            </div>
 
+            </div>
 
         </header>
     )
