85280103af152fed03e735de9fcb03824e88b1d7
Maicol Rojas <maidanlrojas@gmail.com>
2024-08-07 16:34:50 -0500

    Soluciona - Errores en el despligue

--- personal-portfolio/src/App.tsx
+++ personal-portfolio/src/App.tsx
@@ -1,6 +1,3 @@
-/// Librerias de react
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
 /// Paginas
 import { Home } from "./pages/home/Home"
 import { Projects } from "./pages/projects/Projects"
@@ -14,20 +11,6 @@ function App() {
       <Contact />
     </div>
 
-    // <BrowserRouter>
-    //   <Routes>
-
-    //     {/* Path ruta por defecto */}
-    //     <Route path="/" element={<Navigate to="home" />} />
-
-    //     {/* Path de rutas */}
-    //     <Route path="/home" element={<Home />} />
-
-    //     {/* Path ruta no predefinida */}
-    //     <Route path="*" element={<Home />} />
-
-    //   </Routes>
-    // </BrowserRouter>
   )
 }
 
